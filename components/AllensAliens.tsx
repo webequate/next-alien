62f0c23452b23f69f900cbff61fee66eee2d4813
Allen Johnson <webequate@gmail.com>
2023-05-18 01:47:23 -0600

    Adjust SVG title image

--- components/AllensAliens.tsx
+++ components/AllensAliens.tsx
@@ -2,10 +2,10 @@
 const AllensAliens = () => {
   return (
     <svg
-      viewBox="0 0 168 8"
+      viewBox="0 0 168 14"
       className="w-full h-auto mx-auto pt-4 pb-2 sm:pb-2"
     >
-      <text x="0" y="8" fill="currentColor" className="bruno">
+      <text x="0" y="12" fill="currentColor" className="bruno">
         ALLEN&apos;S ALIENS
       </text>
     </svg>
