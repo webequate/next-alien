1d3c4c0fa5c44f5ec56823d0c094077027ed5b59
Allen Johnson <webequate@gmail.com>
2023-05-18 02:35:41 -0600

    Adjust post heading

--- components/PostHeader.tsx
+++ components/PostHeader.tsx
@@ -16,12 +16,13 @@ const PostHeader: React.FC<PostHeaderProps> = ({
   path,
 }) => {
   return (
-    <div className="flex justify-between text-xl sm:text-2xl md:text-3xl">
+    <div className="flex justify-between text-lg sm:text-xl md:text-2xl">
       {prevId ? (
         <Link
           href={`/${path}/${prevId}`}
           title="Previous Post"
           aria-label="Previous Post"
+          className="mr-2"
         >
           <FaArrowLeft className="sm:hover:text-accent-dark sm:dark:hover:text-accent-light" />
         </Link>
@@ -30,14 +31,13 @@ const PostHeader: React.FC<PostHeaderProps> = ({
           <FaArrowLeft />
         </div>
       )}
-      <h2 className="text-xl sm:text-2xl md:text-3xl text-center mb-2">
-        {title}
-      </h2>
+      <h2 className="bruno text-center mb-2">{title}</h2>
       {nextId ? (
         <Link
           href={`/${path}/${nextId}`}
           title="Next Post"
           aria-label="Next Post"
+          className="ml-2"
         >
           <FaArrowRight className="sm:hover:text-accent-dark sm:dark:hover:text-accent-light" />
         </Link>
