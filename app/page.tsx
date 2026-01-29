30cac345a7909e892f67bc2ac4d8ea18f1e2be29
Allen Johnson <webequate@gmail.com>
2026-01-28 17:33:56 -0700

    Remove 1200 x 630 spec from OG image

--- app/page.tsx
+++ app/page.tsx
@@ -24,8 +24,6 @@ export const metadata: Metadata = {
     images: [
       {
         url: "https://allensaliens.com/images/og-alien.jpg",
-        width: 1200,
-        height: 630,
         alt: basics.name,
       },
     ],
